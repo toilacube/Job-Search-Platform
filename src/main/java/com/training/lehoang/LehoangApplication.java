a62082978a805c0b4c24bef986ff0a21e164e79b
toilacube <151115rubikcube@gmail.com>
2024-09-23 16:08:02 +0700

    Init project

---
+++ src/main/java/com/training/lehoang/LehoangApplication.java
@@ -0,0 +1,13 @@
+package com.training.lehoang;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LehoangApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LehoangApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/training/lehoang/LehoangApplicationTests.java
@@ -0,0 +1,13 @@
+package com.training.lehoang;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LehoangApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
