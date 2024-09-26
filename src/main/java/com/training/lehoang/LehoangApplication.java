3266ffdca5a08a5f1e9ae45ed244f22ece9879d0
toilacube <151115rubikcube@gmail.com>
2024-09-26 14:27:17 +0700

    TASK 5: Done testing Mail function

--- src/main/java/com/training/lehoang/LehoangApplication.java
+++ src/main/java/com/training/lehoang/LehoangApplication.java
@@ -1,37 +1,15 @@
 package com.training.lehoang;
 
+import it.ozimov.springboot.mail.configuration.EnableEmailTools;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import com.cloudinary.*;
-import com.cloudinary.utils.ObjectUtils;
-import io.github.cdimascio.dotenv.Dotenv;
 
-import java.io.IOException;
-import java.util.Map;
 @SpringBootApplication
+@EnableEmailTools
 public class LehoangApplication {
 
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         SpringApplication.run(LehoangApplication.class, args);
-
-        // Set your Cloudinary credentials
-
-        Dotenv dotenv = Dotenv.load();
-        Cloudinary cloudinary = new Cloudinary(dotenv.get("CLOUDINARY_URL"));
-        cloudinary.config.secure = true;
-        System.out.println(cloudinary.config.cloudName);
-//
-//
-//// Upload the image
-//        Map params1 = ObjectUtils.asMap(
-//                "use_filename", true,
-//                "unique_filename", false,
-//                "overwrite", true
-//        );
-//
-//        System.out.println(
-//                cloudinary.uploader().upload("https://cloudinary-devs.github.io/cld-docs-assets/assets/images/coffee_cup.jpg", params1));
-    }
+   }
 
 }
